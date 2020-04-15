3f49843090b56580d312090cfe1cbf553361dfe2
Hao Teng <haoteng10@gmail.com>
2020-04-14 23:05:14 -0400

    Use requests from Python

--- tiny_url.py
+++ tiny_url.py
@@ -1,16 +1,11 @@
-from __future__ import with_statement                                                         
-import contextlib 
 try: 
     from urllib.parse import urlencode           
 except ImportError: 
     from urllib import urlencode 
   
-try: 
-    from urllib.request import urlopen 
-except ImportError: 
-    from urllib2 import urlopen 
+
+import requests
   
 def short_url(url):
-    request_url = ('http://tinyurl.com/api-create.php?' + urlencode({'url':url}))    
-    with contextlib.closing(urlopen(request_url)) as response:                       
-        return response.read().decode('utf-8 ')
+    response = requests.get('https://tinyurl.com/api-create.php?' + urlencode({'url':url}))
+    return(response.text)
